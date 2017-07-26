5aff57532925d8b34ab6ad5139eec7aebe715c07
Rick <rickjiangshu@gmail.com>
2017-07-26 17:57:19 +0800

    init

---
+++ Assets/GoodNet/ProtobufSocket.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Author:  Rick
+ * Create:  2017/7/26 17:24:18
+ * Email:   rickjiangshu@gmail.com
+ * Follow:  https://github.com/RickJiangShu
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 使用Protocol Buffer的Socket连接
+/// </summary>
+public class ProtobufSocket : SocketBase
+{
+
+}
